0e315e77ad66b535af8cec2ec392f22de2db3184
DeokJae Kim <kdj0712@gmail.com>
2023-12-13 17:14:57 +0900

    first commit

---
+++ docs/NoSQL/README.md
@@ -0,0 +1,3 @@
+##### MongoDB Functions  ####
+
+- insertOne()   :  db.cars_info.insertOne({....})  | collection에 데이터를 입력할 때 넣는 명령어.
---
+++ docs/NoSQL/mongo_inserts.py
@@ -0,0 +1,9 @@
+# Python to NoSQL DB Flow
+
+# connect to port in MongoDB (Mongo DB의 포트와 접속)
+
+# Database 연결
+
+# collection 작성
+
+# insert
