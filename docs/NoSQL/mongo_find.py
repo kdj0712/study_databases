709af90f276a663ec5aaa19702f0af80acb71108
DeokJae Kim <kdj0712@gmail.com>
2023-12-19 14:07:48 +0900

    quiz_input

--- docs/NoSQL/mongo_find.py
+++ docs/NoSQL/mongo_find.py
@@ -2,9 +2,9 @@
 # class 선정하여 mongo와 연결 (MongoClient = class = MongoCompass)
 from pymongo import MongoClient 
 # connect to port in MongoDB (Mongo DB의 포트와 접속)
-mongoClient = MongoClient("mongodb://localhost:27017") #MongoClient 는 변수(클래스변수)로 담아내서 DB에서 활동할 수 있는 권한을 얻어야 한다.
+# mongoClient = MongoClient("mongodb://localhost:27017") #MongoClient 는 변수(클래스변수)로 담아내서 DB에서 활동할 수 있는 권한을 얻어야 한다.
 # 즉, 자원에 대한 class
-
+mongoClient = MongoClient("mongodb://192.168.0.9:27017")
 # Database 연결
 database = mongoClient["local"]
 
