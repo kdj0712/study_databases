f26df41a7fc0214e54c9997ae3408e4216f54630
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-03-29 15:27:55 +0900

    commit

---
+++ docs/SQLs/quests/whereRangeAndEquals.sql
@@ -0,0 +1,24 @@
+-- REFERS : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_columns
+
+-- SELECT *
+-- FROM Customers
+-- WHERE Country NOT IN ('Germany','USA')
+-- ;
+-- COUNT : 67
+
+
+-- SELECT *
+-- FROM Customers
+-- WHERE (CustomerID BETWEEN 50 AND 89)
+-- AND City = 'Berlin'
+-- ;
+
+-- COUNT : 0
+
+-- SELECT *
+-- FROM Customers
+-- WHERE (CustomerID BETWEEN 50 AND 89)
+-- AND City = 'London'
+-- ;
+
+-- COUNT : 2
